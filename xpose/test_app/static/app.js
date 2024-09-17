47a13aea3fc37620fd553626b2825893ab04d909
arifakhtar51 <arifakhtar902@gmail.com>
2024-09-17 17:05:07 +0530

    a

--- xpose/test_app/static/app.js
+++ xpose/test_app/static/app.js
@@ -1,4 +1,1 @@
-// Additional dummy script
-console.log('This is a dummy script.');
-const GITHUB ACCESS-TOKEN: ghp_VnrmgIADbeQ4QEoy3Zb1PKasGdGgS3Iayvy 
-
+// 
--- xpose/test_app/static/main.js
+++
@@ -1,4 +0,0 @@
-// Contains sensitive information
-
-const AWS_Access_Key-ID : AKIAIOSFODNN7EXAMPLE
-const AWS_Secret_Access_Key : wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY
